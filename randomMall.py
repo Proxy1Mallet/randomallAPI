ebd00bc47d3c43a88b54c96a1a65387ae0fe5924
Proxy Mallet <qq84063@gmail.com>
2022-12-05 20:13:28 +0500

    Update randomMall.py

--- randomMall.py
+++ randomMall.py
@@ -4,53 +4,61 @@ from requests import Session
 
 class randomMall:
     def __init__(self):
-        self.api = 'https://randomall.ru/api/general/{}'.format
+        self.api = 'https://randomall.ru/api/{}'.format
         self.session = Session()
         self.headers = {'user-agent': FakeUserAgent().random}
         self.d = ''.join(choices('abcdefghijklmnopqrstuvwxyz', k=31))
 
     def fantasyName(self):
         data = {'d': self.d}
-        req = self.session.post(url=self.api('fantasy_name'), headers=self.headers, json=data)
+        req = self.session.post(url = self.api('general/fantasy_name'), headers = self.headers, json = data)
         return req.json()
 
     #var is the floor. 1 - male 0 - female
     def appearance(self, var : str):
         data = {'d': self.d}
-        req = self.session.post(url = self.api('appearance'), headers = self.headers, json=data)
+        req = self.session.post(url = self.api('general/appearance'), headers = self.headers, json=data)
         return req.json()
 
     def crowd(self):
         data = {'d': self.d}
-        req = self.session.post(url=self.api('crowd'), headers=self.headers, json=data)
+        req = self.session.post(url = self.api('general/crowd'), headers = self.headers, json = data)
         return req.json()
 
     def character(self):
         data = {'d': self.d}
-        req = self.session.post(url=self.api('character'), headers=self.headers, json=data)
+        req = self.session.post(url = self.api('general/character'), headers = self.headers, json = data)
         return req.json()
 
     def abilities(self):
         data = {'d': self.d}
-        req = self.session.post(url=self.api('abilities'), headers=self.headers, json=data)
+        req = self.session.post(url = self.api('general/abilities'), headers = self.headers, json = data)
         return req.json()
 
     def features(self):
         data = {'d': self.d}
-        req = self.session.post(url=self.api('features'), headers=self.headers, json=data)
+        req = self.session.post(url = self.api('general/features'), headers = self.headers, json = data)
         return req.json()
 
     def jobs(self):
         data = {'d': self.d}
-        req = self.session.post(url=self.api('jobs'), headers=self.headers, json=data)
+        req = self.session.post(url = self.api('general/jobs'), headers = self.headers, json = data)
         return req.json()
 
     def race(self):
         data = {'d': self.d}
-        req = self.session.post(url=self.api('race'), headers=self.headers, json=data)
+        req = self.session.post(url = self.api('general/race'), headers = self.headers, json = data)
         return req.json()
 
     def superpowers(self):
         data = {'d': self.d}
-        req = self.session.post(url=self.api('superpowers'), headers=self.headers, json=data)
+        req = self.session.post(url = self.api('general/superpowers'), headers = self.headers, json = data)
         return req.json()
+
+    def suggestions(self):
+        req = self.session.get(url = self.api('gens/suggestions'), headers = self.headers)
+        return req.json()[0]
+
+    def gensGenerate(self, id):
+        req = self.session.post(url = self.api(f'gens/{id}'), headers = self.headers)
+        return req.json()['msg']
